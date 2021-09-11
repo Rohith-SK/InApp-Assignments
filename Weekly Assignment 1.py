479c5581bfa25ba170dc41e190867ed2cf4058c3
Rohith S K <90336401+Rohith-SK@users.noreply.github.com>
2021-09-11 21:36:21 +0530

    Update Weekly Assignment 1.py

--- Weekly Assignment 1.py
+++ Weekly Assignment 1.py
@@ -1,36 +1,31 @@
-import random
+from random import randint
 "winner"
 user_count=0
 computer_count=0
-available_choices = ["rock", "paper", "scissors"]
-winning_options = [("rock", "scissors"), ("paper", "rock"), ("scissors", "paper")]
+available_choices=["rock","paper","scissors"]
+winning_options = [(0,2), (1,0), (2,1)]
 output_dict={}
 for i in range(1,11):
-    user_choice=int(input("\n1.Rock \n2.Paper \n3.Scissors\nEnter your choice"))
-    if(user_choice==1):
-        user_choice="rock"
-        print("User Choice is :",user_choice)
-    elif(user_choice==2):
-        user_choice="paper"
-        print("User Choice is :", user_choice)
-    elif(user_choice==3):
-        user_choice="scissors"
-        print("User Choice is :", user_choice)
+    user_choice=input("Enter your choice(rock/paper/scissor)")
+    computer_input=available_choices[randint(0,2)]
+    if(user_choice in available_choices):
+        print("Computer Choice is",computer_input)
+        user_input = available_choices.index(user_choice)
+        computer_index=available_choices.index(computer_input)
+        if(user_input==computer_index):
+            user_count=user_count+0
+            computer_count=computer_count+0
+            winner="Nonone"
+        elif((user_input,computer_index) in winning_options):
+            user_count=user_count+1
+            winner="Player"
+        else:
+            computer_count=computer_count+1
+            winner="Computer"
     else:
         print("Invalid Choice")
-    computer_choice=random.choice(available_choices)
-    print("Computer Choice is :",computer_choice)
-    if(user_choice==computer_choice):
-        user_count=user_count+0
-        computer_count=computer_count+0
-        winner="Nonone"
-    elif((user_choice,computer_choice) in winning_options):
-        user_count=user_count+1
-        winner="Player"
-    else:
-        computer_count=computer_count+1
-        winner="Computer"
-    output_dict[i] = [user_choice, computer_choice, winner]
+        winner="Noone"
+    output_dict[i] = [user_choice, available_choices[computer_index], winner]
 print("Points Earned by Player:",user_count)
 print("Points Earned by Computer:",computer_count)
 if(user_count==computer_count):
@@ -38,7 +33,7 @@ if(user_count==computer_count):
 elif(user_count>computer_count):
     print("Overall Winner is Player")
 else:
-    print("Winner is Computer")
+    print("Overall Winner is Computer")
 choice='y'
 while(choice=='y'):
     try:
