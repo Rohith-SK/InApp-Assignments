ca3a4fbfe2c5a22c19b1949cd903230bbbe499e1
Rohith S K <90336401+Rohith-SK@users.noreply.github.com>
2021-11-24 17:15:43 +0530

    Update JS.js

--- jQuery Assignments/Login Using jQuery/JS/JS.js
+++ jQuery Assignments/Login Using jQuery/JS/JS.js
@@ -1,99 +1,236 @@
-$(document).ready(function (){
-    $('#main-navigation-menu').hide();
-    $('.adding-details').hide();
-    $('.main-adding').hide();
-});
+$('#main-navigation-menu').hide();
+$('#main-add-button').hide();
+$('.error-messages').hide();
+
+var firstNameError=false;
+var lastNameError=false;
+var usernameError=false;
+var emailError=false;
+var phoneNumberError=false;
+var passwordError=false;
+var confirmPasswordError=false;
+
+$('#register-first-name').focusout(function (){
+    checkFirstName();
+})
+$('#register-last-name').focusout(function (){
+    checkLastName();
+})
+$('#register-username').focusout(function (){
+    checkUsername();
+})
+$('#register-email').focusout(function (){
+    checkEmail();
+})
+$('#register-phone-number').focusout(function (){
+    checkPhoneNumber();
+})
+$('#register-password').focusout(function (){
+    checkPassword();
+})
+$('#register-confirm-password').focusout(function (){
+    checkConfirmPassword();
+})
 
 
 
+
+
+function checkFirstName() {
+    var pattern = /^[a-zA-Z]*$/;
+    var firstName = $("#register-first-name").val();
+    if (pattern.test(firstName) && firstName !== '') {
+        $('#first-name-error').hide();
+    } else {
+        $('#first-name-error').show();
+        firstNameError = true;
+    }
+ }
+
+ function checkLastName() {
+    var pattern = /^[a-zA-Z]*$/;
+    var lastName = $("#register-last-name").val();
+    if (pattern.test(lastName) && lastName !== '') {
+        $('#last-name-error').hide();
+    } else {
+        $('#last-name-error').show();
+        lastNameError = true;
+    }
+ }
+
+ function checkUsername(){
+     var username=$('#register-username').val();
+     if(username.length>3 && username.length<10 &&username!==''){
+         $('#username-error').hide();
+     }
+     else{
+         $('#username-error').show();
+         usernameError=true;
+     }
+ }
+
+ function checkEmail() {
+    var pattern = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
+    var email = $('#register-email').val();
+    if (pattern.test(email) && email !== '') {
+       $('#email-error').hide();
+    } else {
+       $('#email-error').show();
+       emailError = true;
+    }
+ }
+
+ function checkPhoneNumber(){
+     var phoneNumber=$('#register-phone-number').val();
+     if(phoneNumber.length==10 && phoneNumber!==''){
+         $('#phone-number-error').hide();
+     }
+     else{
+         $('#phone-number-error').show();
+         phoneNumberError=true;
+     }
+ }
+
+ function checkPassword() {
+    var password = $("#register-password").val();
+    if (password.length < 8) {
+       $("#password-error").show();
+       passwordError = true;
+    } else {
+       $("#password-error").hide();
+    }
+ }
+
+ function checkConfirmPassword() {
+    var password = $("#register-password").val();
+    var confirmPassword = $("#register-confirm-password").val();
+    if (password !== confirmPassword) {
+       $("#confirm-password-error").show();
+       confirmPasswordError = true;
+    } else {
+       $("#confirm-password-error").hide();
+    }
+ }
+
+ var registerUser={};
+ var i=0;
+ $(document).ready(function (){
+     $('#register-page-button').click(function (){
+        firstNameError=false;
+        lastNameError=false;
+        usernameError=false;
+        emailError=false;
+        phoneNumberError=false;
+        passwordError=false;
+        confirmPasswordError=false;
+        checkFirstName();
+        checkLastName();
+        checkUsername();
+        checkEmail();
+        checkPhoneNumber();
+        checkPassword();
+        checkConfirmPassword();
+        if (firstNameError === false && lastNameError === false && usernameError === false && emailError === false && phoneNumberError === false && passwordError === false && confirmPasswordError === false) {
+            alert("Registration Successfull");
+            var firstName = $("#register-first-name").val();
+            var lastName = $("#register-last-name").val();
+            var username=$('#register-username').val();
+            var email = $('#register-email').val();
+            var phoneNumber=$('#register-phone-number').val();
+            var password = $("#register-password").val();
+            var confirmPassword = $("#register-confirm-password").val();
+            registerUser[i]=[firstName,lastName,username,email,phoneNumber,password,confirmPassword]
+            i++
+            localStorage.setItem("details",JSON.stringify(registerUser));
+            var details=JSON.parse(localStorage.getItem("details"));
+            $(':input').val('');
+            return true;
+            
+
+         } 
+         else {
+            alert("Please fill the form correctly");
+            return false;
+         }
+     })
+     
+ })
+
+
+var clickHome;
 $(document).ready(function (){
     $('#sign-in').click(function (){
         var username="Rohith";
-        var userpassword="admin";
+        var password="admin";
         var newUsername=$('#login-username').val();
-        var newUserpassword=$('#login-password').val();
-        if(newUsername==='' && newUserpassword===''){
+        var newPassword=$('#login-password').val();
+        localStorage.setItem('name',username);
+        var details=JSON.parse(localStorage.getItem("details"));
+        if(details!==null){
+            var len=Object.keys(details).length;
+        }
+        
+        if(newUsername==='' && newPassword===''){
             alert("Please enter the details");
         }
-        else if(newUsername==='' || newUserpassword===''){
+        else if(newUsername==='' || newPassword===''){
             alert("Please enter the details");
         }
-        else if(newUsername===username && newUserpassword===userpassword){
+        else if(newUsername===username && newPassword===password){
+            clickHome=1;
+            localStorage.setItem('clickHome',clickHome)
             $('#main-navigation-menu').show();
             $('.adding-details').show();
-            $('.main-adding').show();
+            $('#main-add-button').show();
             $('#main-sign-in').hide();
-            $('body').css('background-color','#04703a');
-            $('#main-navigation-menu').css('background-color','white');
-
-        }
-        else if(newUsername===localStorage.username && newUserpassword===localStorage.password){
-            $('#main-navigation-menu').show();
-            $('.adding-details').show();
-            $('.main-adding').show();
-            $('#main-sign-in').hide();
+            $('#sign-in').hide()
+            $('#name').text(' ' + newUsername);
+            $('#profile-name').text(newUsername);
             $('body').css('background-color','#04703a');
             $('#main-navigation-menu').css('background-color','white');
         }
         else{
-            alert("Wrong Username or Password");
+            for(var i=0;i<len;i++){
+                if(newUsername===details[i][2] && newPassword===details[i][5]){
+                    localStorage.setItem('clickHome',clickHome)
+                    clickHome=2;
+                    $('#main-navigation-menu').show();
+                    $('.adding-details').show();
+                    $('#main-add-button').show();
+                    $('#main-sign-in').hide();
+                    $('#sign-in').hide()
+                    $('#name').text(' ' + details[i][0]);
+                    $('#profile-name').text(details[i][0]);
+                    localStorage.setItem('name1',details[i][0])
+                    $('body').css('background-color','#04703a');
+                    $('#main-navigation-menu').css('background-color','white');
+                }
+                if(i==len-1 && newUsername!==details[i][2] && newPassword!==details[i][5]){
+                    alert('Wrong Username or Password')
+                }
+            }
         }
-    });
-});
+    
+    })
+})
 
 $(document).ready(function (){
-    var registerUser={};
-    var i=0;
-    $('#register-page-button').click(function (){
-        var inputName=document.getElementById("register-username").value;
-    var inputPassword=document.getElementById("register-password").value;
-    if(inputName==='' && inputPassword===''){
-        alert("Please enter the details");
-    }
-    else if(inputName==='' || inputPassword===''){
-        alert("Please enter the details");
+    var clickHome=localStorage.getItem('clickHome')
+    if(clickHome==1){
+        var name=localStorage.getItem('name')
+        $('#name1').text(name)
+        console.log(name)
+        $('#profile-name1').text(name)
     }
     else{
-        alert("Registration is Successful");
-        registerUser[i]=[inputName,inputPassword];
-        i++;
-        localStorage.setItem("username", inputName);
-        var username=localStorage.getItem("username");
-        localStorage.setItem("password", inputPassword);
-        var password=localStorage.getItem("password");
-    } 
-    });
-});
-
-var finalList={};
+        var name1=localStorage.getItem('name1');
+        $('#name1').text(name1);
+        $('#profile-name1').text(name1);
+    }
+})
+    
 $(document).ready(function (){
-    var j=0;
-    var k =0;
-    $('#add-button').click(function (){
-        var listArray=[];
-        var value=$('#add-username').val();
-        var value1=$('#add-email').val();
-        var value2=$('#add-password').val();
-        var value3=$('#add-phone-number').val();
-        var value4=$('#add-address').val();
-        var value5=$('#add-pincode').val();
-        if(value==='' || value1==='' || value2==='' || value3==='' || value4==='' || value5===''){
-            alert("Please enter the details");
-        }
-        else{
-            listArray.push(value,value1,value2,value3,value4,value5);
-            k++;
-            finalList[j]=JSON.stringify(listArray);
-            j++;
-            $('#new-user').append('<ul id="list"></ul>');
-            $('#list').append("Details of User"+" "+k);
-            for(var i=0;i<listArray.length;i++){
-                $('#list li').each(function (i){
-                    $(this).attr('id', 'list-details' + (i+1))
-                })
-                $('#list').append('<li>' + listArray[i] + '</li>' + '<hr />');
-            }
-            $(':input').val('');
-        }
-    });
-});
+    $('#navigation-menu-item-home').click(function(){
+        window.open("index1.html");
+    }); 
+})
